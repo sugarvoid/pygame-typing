e0f22127b7af47da7d09b12fe53e532167a071bf
sugarvoid <gamedev@kitargo.com>
2023-08-05 11:05:47 -0500

    Change title

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ tile_size = 8
 game_screen = Vector2(128,128)
 
 
-title = 'Island Adventure'
+title = 'Pygame - Typing'
 scale = 6
 screen_width, screen_height = (game_screen.x*scale, game_screen.y*scale)
 bg_color = (0, 0, 0)
